25c7e40d1bb981fb1761c7bdb39dbf0266a55fcf
Sazid Ansari <sazidansari9634@gmail.com>
2022-12-06 00:53:40 +0545

    customers-list data successfully fatch

--- lib/auth/users_page.dart
+++ lib/auth/users_page.dart
@@ -1,11 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
 
+  final String userlist = r"""
+                          query {
+                            customers{
+                              id
+                              email
+                              firstName
+                            }
+                          }
+                          """;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Customers-List"),
+      ),
+      body: Query(options: QueryOptions(
+              document: gql(userlist), 
+              ),
+
+          
+          builder: (result, {fetchMore, refetch}) {
+            if (result.isLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (result.data == null) {
+              return const Center(
+                child: Text("Data Not Found!!!"),
+              );
+            }
+            return ListView.builder(
+                itemCount: result.data!['customers'].length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      result.data!['customers'][index]["firstName"],
+                    ),
+                    subtitle: Text(
+                        result.data!['customers'][index]["email"]),
+                  );
+                });
+          }),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,29 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initHiveForFlutter();
-  final HttpLink link = HttpLink('https://cfc1-2407-5200-402-aea5-2ceb-114-bc95-2adb.in.ngrok.io/graphql');
-  ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(link: link, cache: GraphQLCache(store: HiveStore()))
-  );
-  runApp( MyApp(client: client,));
+
+  runApp( MaterialApp(title: 'GraphQL Auth', home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  final ValueNotifier<GraphQLClient> client;
-  const MyApp({Key? key,required this.client }) : super(key: key);
+  const MyApp({Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+  final HttpLink httpLink = HttpLink('https://cfc1-2407-5200-402-aea5-2ceb-114-bc95-2adb.in.ngrok.io/graphql',
+  defaultHeaders: <String, String>{'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NTUsImV4cCI6MTY3NDk5NjAyNH0.5cLNyUZDauGWbuTzQoADMwU4F7tUuWDol_kdHI3a9wM'}
+  );
+  final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+    GraphQLClient(
+      link: httpLink,
+      cache: GraphQLCache(
+        store: HiveStore()
+      )
+    )
+  );
+ 
     return GraphQLProvider(
       client: client,
-      child: CacheProvider(
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Flutter GraphQL Demo',
@@ -28,7 +35,6 @@ class MyApp extends StatelessWidget {
           ),
           home: const WelcomeScreen(),
         ),
-      ),
     );
   }
 }
