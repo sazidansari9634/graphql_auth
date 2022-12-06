26549f8405ad04a89213f8dcd65d311251e55e38
Sazid Ansari <sazidansari9634@gmail.com>
2022-12-06 18:40:09 +0545

    added the logout button on userscreen

--- lib/auth/users_page.dart
+++ lib/auth/users_page.dart
@@ -1,5 +1,8 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_auth/welcome.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
@@ -18,7 +21,27 @@ class UserScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text("Customers-List"),
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(icon: Icon(Icons.logout_outlined), onPressed: ()async{
+          ScaffoldMessenger.of(context).showSnackBar(
+             SnackBar(
+                      elevation: 0,
+                      backgroundColor: Colors.transparent,
+                      content: 
+                      AwesomeSnackbarContent(title: " Logout Successfully",
+                       message: "",
+                        contentType: ContentType.help)
+                    ),
+          );
+          final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+          sharedPreferences.remove('token');
+          Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=> WelcomeScreen()), (route) => false);
+          })
+        ],
+
       ),
       body: Query(options: QueryOptions(
               document: gql(userlist), 
